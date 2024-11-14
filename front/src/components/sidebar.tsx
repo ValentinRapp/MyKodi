4fd53d070cefc45dd39d3ed56936739f2beea1a6
valentin <valentin.rapp@epitech.eu>
2024-11-14 13:22:32 +0100

    complete implementation of settings page

--- front/src/components/sidebar.tsx
+++ front/src/components/sidebar.tsx
@@ -3,19 +3,29 @@ import { NavLink, Outlet } from "react-router-dom";
 export function Sidebar() {
   return (
     <div className="drawer lg:drawer-open">
-      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center justify-center">
+      <input id="drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex justify-center">
         <Outlet />
-        <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
-          Open drawer
+        <label htmlFor="drawer" className="btn btn-square btn-ghost absolute left-2 top-2 lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-current">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
         </label>
       </div>
       <div className="drawer-side">
-        <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+        <label htmlFor="drawer" aria-label="close sidebar" className="drawer-overlay"></label>
         <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
           <div className="my-4 flex justify-center">
             <img src="/kodi-logo.webp" alt="logo" width={50} height={50} />
-            <h1 className="text-4xl mt-2" style={{fontFamily: "Helvetica-rounded-bold"}} >EpiKodi</h1>
+            <h1 className="text-4xl mt-2" style={{ fontFamily: "Helvetica-rounded-bold" }} >EpiKodi</h1>
           </div>
           <li>
             <NavLink
--- front/src/router/Settings.tsx
+++ front/src/router/Settings.tsx
@@ -1,3 +1,109 @@
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+type Source = {
+  path: string;
+  id: string;
+}
+
+function TextEdit(props: {
+  value: string,
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  saveCallback: (value: string) => void
+}) {
+  return (
+    <input
+      type="text"
+      placeholder="Write Path"
+      value={props.value}
+      onChange={e => props.onChange(e)}
+      onBlur={() => props.saveCallback(props.value)}
+      onKeyDown={e => {
+        if (e.key === "Enter") {
+          props.saveCallback(props.value);
+        }
+      }}
+      className="input input-bordered w-full max-w-xs"
+    />
+  )
+}
+
 export function Settings() {
-    return <h1>Settings</h1>
+
+  const [sources, setSources] = useState<Source[]>([]);
+
+  useEffect(() => {
+    fetchSources();
+  }, []);
+
+  const fetchSources = async () => {
+    const paths = await fetch("http://localhost:3000/paths").then(res => res.json());
+    setSources(paths.paths.map((path: string) => ({ path, id: uuidv4() })));
+  }
+
+  const addSource = async (path: string) => {
+
+    const paths = sources.map(s => s.path);
+    const newPaths = Array.from(new Set(paths));
+    
+    setSources(newPaths.map((path: string) => ({ path, id: uuidv4() })));
+
+    if (paths.length !== newPaths.length) {
+      return;
+    }
+
+    const res = await fetch("http://localhost:3000/paths/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ path })
+    });
+
+    if (!res.ok) {
+      setSources(sources.filter(s => s.path !== path));
+      alert("Invalid path");
+    }
+  }
+
+  return (
+    <div>
+      <h1 className="text-6xl m-6" >Settings</h1>
+      <div>
+        <h2 className="text-4xl" >Sources</h2>
+        <ul>
+          {sources.map(source => (
+            <li key={source.id} className="flex my-2">
+              <TextEdit
+                value={source.path}
+                onChange={e => setSources(sources.map(s => s.id === source.id ? { ...s, path: e.target.value } : s))}
+                saveCallback={addSource}
+              />
+              <button
+                className="btn btn-square btn-error ml-2"
+                onClick={() => {
+                  setSources(sources.filter(s => s.id !== source.id));
+                  fetch(`http://localhost:3000/paths/remove`, {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ path: source.path })
+                  });
+                }}
+              >
+                🗑️
+              </button>
+            </li>
+          ))}
+          <button
+            className="btn btn-square btn-primary w-full"
+            onClick={() => setSources([...sources, { path: "", id: uuidv4() }])}
+          >
+            +
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
 }
