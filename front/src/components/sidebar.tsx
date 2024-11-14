8b412a123dd1a526357db33206f54fa105e2182c
valentin <valentin.rapp@epitech.eu>
2024-11-14 11:11:22 +0100

    logo in sidebar

--- front/src/components/sidebar.tsx
+++ front/src/components/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export function Sidebar() {
   return (
@@ -13,8 +13,31 @@ export function Sidebar() {
       <div className="drawer-side">
         <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
         <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/settings">Settings</Link></li>
+          <div className="my-4 flex justify-center">
+            <img src="/kodi-logo.webp" alt="logo" width={50} height={50} />
+            <h1 className="text-4xl mt-2" style={{fontFamily: "Helvetica-rounded-bold"}} >EpiKodi</h1>
+          </div>
+          <li>
+            <NavLink
+              to="/"
+              end
+              className={({ isActive }) =>
+                isActive ? "active text-primary" : ""
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/settings"
+              className={({ isActive }) =>
+                isActive ? "active text-primary" : ""
+              }
+            >
+              Settings
+            </NavLink>
+          </li>
         </ul>
       </div>
     </div>
