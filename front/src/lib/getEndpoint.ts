b4680d3695ac665cdac48c79cf62969a1ea72835
ValentinRapp <valentin.rapp@epitech.eu>
2025-07-28 15:27:26 +0200

    demo branch initial commit

--- front/src/lib/getEndpoint.ts
+++ front/src/lib/getEndpoint.ts
@@ -1,7 +1,4 @@
 export const getEndpoint = (): string => {
     const endpointData = JSON.parse(localStorage.getItem('endpoint_address') || JSON.stringify({ address: "localhost", port: "2425" }));
-    if (endpointData.address.startsWith('https://')) {
-        return `${endpointData.address}`;
-    }
     return `http://${endpointData.address}:${endpointData.port}`;
 }
