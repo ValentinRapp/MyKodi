eee7cdd23271667ed1e8dd6b464673f986ed3888
ValentinRapp <valentin.rapp@epitech.eu>
2026-01-16 00:51:27 +0000

    fix

--- front/src/lib/getEndpoint.ts
+++ front/src/lib/getEndpoint.ts
@@ -1,4 +1,3 @@
 export const getEndpoint = (): string => {
-    const endpointData = JSON.parse(localStorage.getItem('endpoint_address') || JSON.stringify({ address: "localhost", port: "2425" }));
-    return `http://${endpointData.address}:${endpointData.port}`;
+    return "https://mykodiserver.valentinrapp.fr";
 }
